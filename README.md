8a4d83a9faa2417db9785a503fd7846dda433d2b
dencaval <dencaval@gmail.com>
2016-09-28 02:47:23 -0300

    First version for reviewing

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # popular_movies_stage_1
 
-- Please, update TMDB_API_KEY constant at Utils.java with your your TMDB API key.
+- Please, update TMDB_API_KEY constant at Utils.java with your TMDB API key.
--- popular_movies_stage_1/app/src/main/java/com/dencaval/project01/SettingsActivity.java
+++ popular_movies_stage_1/app/src/main/java/com/dencaval/project01/SettingsActivity.java
@@ -3,7 +3,6 @@ package com.dencaval.project01;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.widget.Toast;
 
 /**
  * Created by denis on 21/08/2016.
@@ -17,13 +16,10 @@ public class SettingsActivity extends PreferenceActivity implements
         addPreferencesFromResource(R.xml.preferences);
         Preference pref = findPreference(getString(R.string.pref_sorting_key));
         pref.setOnPreferenceChangeListener(this);
-
-//        this.onPreferenceChange(pref, PreferenceManager.getDefaultSharedPreferences(pref.getContext()).getString(pref.getKey(), ""));
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object o) {
-        Toast.makeText(getApplicationContext(), o.toString(), Toast.LENGTH_SHORT).show();
         return true;
     }
 }
