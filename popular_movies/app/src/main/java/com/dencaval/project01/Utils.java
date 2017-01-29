a168a9f15d3ac53e7da692998e980b511e0e150d
dencaval <dencaval@gmail.com>
2017-01-29 17:25:49 -0300

    Remove example api key

--- popular_movies/app/src/main/java/com/dencaval/project01/Utils.java
+++ popular_movies/app/src/main/java/com/dencaval/project01/Utils.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
  * Created by denis on 06/09/2016.
  */
 public class Utils {
-    final static public String TMDB_API_KEY = "91aac8f2720c38e2a19de85f21271430";
+    final static public String TMDB_API_KEY = "";
 
     public final static String CRITERIA_POPULAR = "0";
     public final static String CRITERIA_USER_RATING = "1";
